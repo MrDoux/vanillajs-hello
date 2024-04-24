b8b8174933f201e1c65d73229cda2c7d6ba0a646
Douglas Melendez <160060653+MrDoux@users.noreply.github.com>
2024-04-24 16:33:30 +0000

    se creo el generador de escusas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function GeneratorEscuse(who, action, what, when) {
+  let numberWho = Math.floor(Math.random() * who.length);
+  let numberAction = Math.floor(Math.random() * action.length);
+  let numberWhat = Math.floor(Math.random() * what.length);
+  let numberWhen = Math.floor(Math.random() * when.length);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  return `${who[numberWho]} ${action[numberAction]} ${what[numberWhat]} ${when[numberWhen]}`;
+}
+
+function onLoad() {
+  let excuseLink = document.getElementById("excuse");
+  excuseLink.innerHTML = GeneratorEscuse(who, action, what, when);
+}
+window.onload = onLoad;
